a08c5fc83dc71a341738726abc03fd12d67d25f7
dali2219 <dali13132018@gmail.com>
2020-06-22 23:41:52 -0400

    all django

---
+++ helloworld_1/helloworld/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from datetime import datetime
+
+def index(request):
+  context = {
+    'current_date':datetime.now(),
+    'title':'Home'
+  }
+  return render(request,'index.html',context)
+
+def about(request):
+  context = {
+    'current_date':datetime.now(),
+    'title':'About'
+  }
+  return render(request,'about.html',context)
+
+def news(request):
+  context = {
+    'current_date':datetime.now(),
+    'title':'news'
+  }
+  return render(request,'news.html',context)
